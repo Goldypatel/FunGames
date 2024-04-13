2a43624f69486f2d04398cadadb3dced9dc8fb96
Goldy <112779392+Goldypatel@users.noreply.github.com>
2024-04-13 23:45:17 +0530

    added console based QR generator

---
+++ index.js
@@ -0,0 +1,35 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from 'fs';
+
+inquirer.prompt([{
+
+    name:'URL',
+    message:'please enter the link'
+}
+  ])
+  .then((answers) => {
+    const url=answers.URL;
+ 
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_dl.png'));
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("there some error")
+    } else {
+      // Something else went wrong
+    }
+  });
+ 
+// var qr_svg = qr.image('I love QR!', { type: 'svg' });
+// qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+ 
+// var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
